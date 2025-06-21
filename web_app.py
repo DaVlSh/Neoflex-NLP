ae1ac9852ce1cba68a6aef568d9c76e3e775306a
= <=>
2025-06-21 15:04:20 +0500

    add web_app.py

---
+++ web_app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from joblib import load
+from transformers import BertTokenizer
+import pandas as pd
+import torch
+
+
+def load_model():
+    model = load('model.joblib')
+    model.eval()
+
+    return model
+
+def prediction(data):
+    data = data['TEXTVALUE']
+    tokenizer = BertTokenizer.from_pretrained('DeepPavlov/rubert-base-cased-sentence')
+
+    tokens_test = tokenizer.batch_encode_plus(
+        data.tolist(),
+        max_length=15,
+        padding='max_length',
+        truncation=True,
+        return_tensors='pt'
+    )
+
+    test_mask = tokens_test['attention_mask']
+    test_seq = tokens_test['input_ids']
+
+    with torch.no_grad():
+        preds = model(test_seq, test_mask)
+        predicted_classes = torch.argmax(preds, dim=1).cpu().numpy()
+
+    return predicted_classes
+
+def load_file():
+    uploaded_file = st.file_uploader(label='Выберите csv файл', type='csv')
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+        return data[['TEXTVALUE']]
+    else:
+        return None
+
+
+model = load_model()
+
+st.title('Система предсказания удовлетворенности сотрудников')
+file = load_file()
+result = st.button('Предсказать категории удовлетворенности')
+if result:
+    predictions = prediction(file)
+    dataframe = file
+    dataframe['Satisfaction'] = predictions
+    st.write(dataframe)
